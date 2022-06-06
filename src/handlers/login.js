2504715f178798e23e7a7de348a84afd21032409
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2022-06-06 10:08:03 +0200

    Remove lodash dependency

--- src/handlers/login.js
+++ src/handlers/login.js
@@ -1,15 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
 import jwt from "jsonwebtoken";
-import pkg from "lodash";
-const { get } = pkg;
 import * as githubServices from '../services/github.js';
 
 const COOKIE_NAME = process.env.COOKIE_NAME;
 const COOKIE_SECRET = process.env.COOKIE_SECRET;
 
 async function decodeToken(req, res) {
-  const cookie = get(req, `cookies[${COOKIE_NAME}]`);
+  const cookie = req.cookies[COOKIE_NAME];
 
   try {
     const decode = jwt.verify(cookie, COOKIE_SECRET);
